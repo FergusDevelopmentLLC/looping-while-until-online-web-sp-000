7d7b15b58018de07deb94c500fbba71d1f0549f7
Will Carter <will.carter@fergusllc.com>
2019-12-13 20:40:29 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,15 +1,11 @@
 def using_until
   levitation_force = 6
-  #your code here
-    
-end
-
-
-until var == 11 do
   
-  # code to be executed 
-  puts var * 10
-  var = var + 1
-    
-# here loop ends 
+  until levitation_force == 11 do
+    levitation_force = levitation_force + 1
+  end
+  
 end
+
+
+
